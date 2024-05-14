3845f368daaa902a74bbb3632f1be163cad831be
Jayashri mahale <75965534+jayashrimahale@users.noreply.github.com>
2024-05-14 14:32:00 +0530

    Update database resources.tf

--- database resources.tf
+++ database resources.tf
@@ -10,12 +10,12 @@ resource "aws_db_instance" "three-tier-db" {
   allocated_storage           = 100
   storage_type                = "gp3"
   engine                      = "mysql"
-  engine_version              = "8.0"
+  engine_version              = "8.0.35"
   instance_class              = "db.t2.micro"
   identifier                  = "three-tier-db"
   username                    = "admin"
-  password                    = "23vS5TdDW8*o"
-  parameter_group_name        = "default.mysql8.0"
+  password                    = "admin"
+  parameter_group_name        = "default.mysql8.0.35"
   db_subnet_group_name        = aws_db_subnet_group.three-tier-db-sub-grp.name
   vpc_security_group_ids      = ["${aws_security_group.three-tier-db-sg.id}"]
   #availability_zone           = "ap-southeast-2a"
