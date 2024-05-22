461e152869f2a3020de47ed7c29f6035a537466d
Jayashri mahale <75965534+jayashrimahale@users.noreply.github.com>
2024-05-22 14:10:33 +0530

    Update network resources.tf

--- network resources.tf
+++ network resources.tf
@@ -52,7 +52,7 @@ resource "aws_subnet" "three-tier-pvt-sub-2" {
 resource "aws_subnet" "three-tier-pvt-sub-3" {
   vpc_id                  = aws_vpc.three-tier-vpc.id
   cidr_block              = "10.0.0.64/28"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
   tags = {
     Name = "three-tier-pvt-sub-3"
