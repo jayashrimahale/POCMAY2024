ef20c241e5f8b55fb5b3dfff699eaffd8703cc98
Jayashri mahale <75965534+jayashrimahale@users.noreply.github.com>
2024-05-28 18:32:31 +0530

    Update database resources.tf

--- database resources.tf
+++ database resources.tf
@@ -13,8 +13,10 @@ resource "aws_db_instance" "three-tier-db" {
   engine_version              = "8.0"
   instance_class              = "db.t3.micro"
   identifier                  = "three-tier-db"
-  username                    = data.vault_generic_secret.db_credentials.data["username"]
-  password                    = data.vault_generic_secret.db_credentials.data["password"]
+  #username                    = data.vault_generic_secret.db_credentials.data["username"]
+  #password                    = data.vault_generic_secret.db_credentials.data["password"]
+   username                     =admin
+   password                     ="mypass"
   parameter_group_name        = "default.mysql8.0"
   db_subnet_group_name        = aws_db_subnet_group.three-tier-db-sub-grp.name
   vpc_security_group_ids      = ["${aws_security_group.three-tier-db-sg.id}"]
